176f2524c70fdeb716c4ffc92e1fa2446838ee56
ssogawula <sonwabile.sogawula@gmail.com>
2017-11-27 12:01:23 +0200

    adding unit test

--- src/test/java/com/converter/sever/ConverterControllerTest.java
+++ src/test/java/com/converter/sever/ConverterControllerTest.java
@@ -1,43 +1,23 @@
 package com.converter.sever;
 
-import org.apache.catalina.filters.CorsFilter;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.converter.sever.conf.WebConfig;
 import com.converter.sever.controller.ConverterController;
-import com.converter.sever.dto.ConvertDTO;
 import com.converter.sever.service.ConverterService;
-import com.converter.sever.util.Unit;
 
-import junit.framework.Assert;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -77,8 +57,6 @@ public class ConverterControllerTest {
 				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
 		.andExpect(status().isOk())
 		.andExpect(content().contentType("application/json;charset=UTF-8"));
-		
-		
 	}
 	
 	@Test
